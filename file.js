52e47d97f15b3ccdb40f83164c9167d9470ecd12
=anurag08-coder <anuragharsh626@gmail.com>
2021-04-12 12:03:14 +0530

    Added the CSS file

---
+++ file.js
@@ -0,0 +1,14 @@
+/* Function to Reset the game */
+function Function2(){
+    location.reload();
+    document.getElementById('b1').innerHTML="";
+    document.getElementById('b2').innerHTML="";
+    document.getElementById('b3').innerHTML="";
+    document.getElementById('b4').innerHTML="";
+    document.getElementById('b5').innerHTML="";
+    document.getElementById('b6').innerHTML="";
+    document.getElementById('b7').innerHTML="";
+    document.getElementById('b8').innerHTML="";
+    document.getElementById('b9').innerHTML="";
+}
+
